03a1d926bdc4db845e0dc9df2b4500028ce0816a
Rambros <rambrosteam@users.noreply.github.com>
2024-09-11 22:04:26 +0530

    create model files

---
+++ src/RCX_model.h
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#ifndef RCX_MODELS_H
+#define RCX_MODELS_H
+
+enum ModelType : uint8_t {
+  RC_TRUCK,
+  RC_CAR,
+  RC_EXCAVATOR,
+  RC_LOCOMOTIVE,
+  RC_TANK,
+  RC_TRAIN,
+  RC_SCOOTER,
+};
+
+class RCX_Model {
+public:
+  RCX_Model();
+};
---
+++ src/RCX_models.cpp
@@ -0,0 +1,1 @@
+#include "RCX_model.h"
