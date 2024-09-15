be733a235320624a53116d06b4c4b4f22d5819a6
Rambros <rambrosteam@users.noreply.github.com>
2024-09-15 20:31:09 +0530

    added basic digital functions for led

--- src/RCX_lights.cpp
+++ src/RCX_lights.cpp
@@ -2,83 +2,106 @@
 #include "RCX_lights.h"
 
 RCX_Lights::RCX_Lights() {
-  leds = nullptr;
+  //leds = nullptr;
   numLeds = 0;
   ledResoltuion = ((1 << DefaultLedResolution) - 1);
 }
-void RCX_Lights::addLed(LedType type, int8_t pin, int8_t brightness) {
-  Led *newLeds = (Led *)realloc(leds, (numLeds + 1) * sizeof(Led));
-  if (newLeds == nullptr) {
-    return;  // Handle memory allocation error
+
+void RCX_Lights::addLed(LedType type, int8_t pin) {
+  // search for the first free pin in the type array
+  for (uint8_t i = 0; i < 4; i++) {
+    if (leds[type].pin[i] == 127) {
+      // save the pin and break the loop
+      leds[type].pin[i] = pin;
+      break;
+    }
   }
-  leds = newLeds;
-  leds[numLeds].type = type;
-  if (pin < 0) {
-    leds[numLeds].invert = true;
-    pin = -pin;
-  } else {
-    leds[numLeds].invert = false;
-  }
-  leds[numLeds].ledState = 0;
-  leds[numLeds].pin = pin;
-  leds[numLeds].ledDutyCycle = (brightness * ledResoltuion / 100);
-  numLeds++;
-  ledcAttachChannel(pin, DefaultLedFrequency, DefaultLedResolution, numLeds);
-  if (leds[numLeds].invert) ledcOutputInvert(pin, true);  // use ledcOutputInvert functionality
+  // set the channel to invalid
+  leds[type].channel = 127;
+  // set the pin mode to output
+  pinMode(abs(pin), OUTPUT);
 }
 
+void RCX_Lights::removeLed(int8_t pin) {
+  for (uint8_t i = 0; i < 10; i++) {
+    for (uint8_t j = 0; j < 4; j++) {
+      if (leds[i].pin[j] == pin) {
+        // remove the pin and break the loop
+        ledcDetach(abs(leds[i].pin[j]));
+        leds[i].pin[j] = 127;
+        break;
+      }
+    }
+  }
+}
+
+void RCX_Lights::_attachPWM(LedType type) {
+  if (leds[type].channel != 127) return;
+  uint8_t channel = 0;
+  while (channelBitMask & (1 << channel)) {
+    channel++;
+  }
+
+  for (uint8_t i = 0; i < 4; i++) {
+    if (leds[type].pin[i] != 127) {
+      leds[type].channel = channel;
+      ledcAttachChannel(abs(leds[type].pin[i]), DefaultLedFrequency, DefaultLedResolution, channel);
+      if (leds[type].pin[i] < 0) ledcOutputInvert(abs(leds[type].pin[i]), true);
+    }
+  }
+  channelBitMask |= (1 << channel);
+}
+void RCX_Lights::_detachPWM(LedType type) {
+}
+
+void RCX_Lights::digitalState(LedType type, bool state) {
+  if (leds[type].channel != 127) _detachPWM(type);
+  for (auto &pin : leds[type].pin) {
+    if (pin != 127) {
+      if (pin < 0) {
+        digitalWrite(abs(pin), !state);
+      } else {
+        digitalWrite(pin, state);
+      }
+    }
+  }
+}
 void RCX_Lights::turnOn(LedType type) {
-  updateLed(type, HIGH);
+  digitalState(type, true);
+}
+void RCX_Lights::turnOff(LedType type) {
+  digitalState(type, false);
 }
 
-void RCX_Lights::turnOff(LedType type) {
-  updateLed(type, LOW);
+void RCX_Lights::setBrightness(LedType type, uint8_t brightness) {
+  uint16_t _ledDutyCycle = (brightness * ledResoltuion) / 100;
+  setPwmDuty(type, _ledDutyCycle);
+}
+void RCX_Lights::setPwmDuty(LedType type, uint16_t ledDutyCycle) {
+  uint8_t channel = leds[type].channel;
+  if (channel == 127) _attachPWM(type);
+  ledcWriteChannel(channel, ledDutyCycle);
+}
+
+void RCX_Lights::applyFade(LedType type, bool state, uint16_t fadeTime) {
+  if (leds[type].channel == 127) _attachPWM(type);
+  if (state == 1) {
+    ledcFade(leds[type].pin[0], 0, ledResoltuion, fadeTime);
+  } else {
+    ledcFade(leds[type].pin[0], ledResoltuion, 0, fadeTime);
+  }
 }
 
 void RCX_Lights::fadeOn(LedType type, uint16_t fadeTime) {
-  updateLed(type, HIGH, fadeTime);
+  applyFade(type, HIGH, fadeTime);
 }
 
 void RCX_Lights::fadeOff(LedType type, uint16_t fadeTime) {
-  updateLed(type, LOW, fadeTime);
+  applyFade(type, LOW, fadeTime);
 }
-
-void RCX_Lights::setBrightness(LedType type, int8_t brightness) {
-  for (uint8_t i = 0; i < numLeds; i++) {
-    if (leds[i].type == type) {
-      leds[i].ledDutyCycle = (brightness * ledResoltuion / 100);
-    }
-  }
-}
-
-bool RCX_Lights::getLedState(LedType type) {
-  uint8_t ledNum = type;
-  return leds[ledNum].ledState;
-}
-
-void RCX_Lights::blink(LedType type, uint16_t onTime, uint16_t offTime) {
-}
-
-
-void RCX_Lights::updateLed(LedType type, bool ledstate, uint16_t fadeTime) {
-  for (uint8_t i = 0; i < numLeds; i++) {
-    if (leds[i].type == type) {
-      updateLed(i, ledstate, fadeTime);
-    }
-  }
-}
-void RCX_Lights::updateLed(uint8_t led_id, bool ledstate, uint16_t fadeTime) {
-  leds[led_id].ledState = ledstate;
-  if (fadeTime == 0) {
-    ledcWrite(leds[led_id].pin, (ledstate ? 0 : leds[led_id].ledDutyCycle));
-  } else {
-    ledcFade(leds[led_id].pin, (ledstate ? 0 : leds[led_id].ledDutyCycle), (ledstate ? leds[led_id].ledDutyCycle : 0), fadeTime);
-  }
-}
-
 void RCX_Lights::breathe(LedType type, int16_t breatheTime, bool defaultState, bool repeatAgain) {
 }
-
-
 void RCX_Lights::breathing(LedType type, int16_t breatheTime, int16_t delayTime) {
+}
+void RCX_Lights::blink(LedType type, uint16_t onTime, uint16_t offTime) {
 }
--- src/RCX_lights.h
+++ src/RCX_lights.h
@@ -4,84 +4,88 @@
 #define RCX_LIGHTS_H
 
 #define DefaultLedFrequency 4000
-#define DefaultLedResolution 8
+#define DefaultLedResolution 10
 #define DefaultFadeTime 500
 
-// ESP32S3 has only 8 ledc channels
-enum LedType {
+#define maxChannels 8  // ESP32S3 has only 8 ledc channels
+
+enum LedType : uint8_t {
   // common lights
-  HEAD_LIGHT,
-  FULLBEAM_LIGHT,
-  TAIL_LIGHT,
-  BRAKE_LIGHT,
-  LEFT_INDICATOR,  // left and right indicator will use the same ledc channel
-  RIGHT_INDICATOR,
-  CAB_LIGHT,
-  FOG_LIGHT,
-  REVERSE_LIGHT,
+  HEAD_LIGHT_ID = 1,
+  FULLBEAM_LIGHT_ID = 2,
+  TAIL_LIGHT_ID = 3,
+  BRAKE_LIGHT_ID = 4,
+  LEFT_INDICATOR_ID = 5,  // left and right indicator can use the same ledc channel
+  RIGHT_INDICATOR_ID = 6,
+  CAB_LIGHT_ID = 7,
+  FOG_LIGHT_ID = 8,
+  REVERSE_LIGHT_ID = 9,
   // used in trucks
-  SIDE_LIGHT,
-  BEACON_LIGHT,
-  HAZARD_LIGHT,
+  SIDE_LIGHT_ID = 10,
+  BEACON_LIGHT_ID = 11,
+  HAZARD_LIGHT_ID = 12,
   // exclusively used in locomotives
-  WALKWAY_LIGHT,
-  DITCH_LIGHT,
-  // extra leds
-  LED1,
-  LED2,
-  LED3,
-  LED4,
-  LED5,
-  LED6,
-};
-enum LedStates : uint8_t {
-  LED_OFF = 0,
-  LED_ON = 1,
-  LED_BLINKING = 2,
-  LED_BREATHING = 3,
+  WALKWAY_LIGHT_ID = 13,
+  DITCH_LIGHT_ID = 14,
+  // extra leds with same types
+  LED1 = 1,
+  LED2 = 2,
+  LED3 = 3,
+  LED4 = 4,
+  LED5 = 5,
 };
 
-struct Led {
-  LedType type;
-  int8_t pin;
-  bool invert;
-  bool ledState;
-  uint8_t brightness;
-  int16_t onTime;
-  int16_t offTime;
-  int16_t smoothTime = 500;
-  int16_t ledDutyCycle;
-  int16_t ledFrequency;
+enum LedState : uint8_t {
+  ON = 1,
+  OFF = 0,
 };
-
 class RCX_Lights {
 public:
   RCX_Lights();
   void addLed(LedType type, int8_t pin);
-  void addLed(LedType type, int8_t pin, int8_t brightness);
+  void removeLed(int8_t pin);
 
-  void updateLed(LedType type, bool ledstate, uint16_t fadeTime = 0);
-  void updateLed(uint8_t led_id, bool ledstate, uint16_t fadeTime = 0);
 
   void turnOn(LedType type);
   void turnOff(LedType type);
+  void digitalState(LedType type, bool state);
 
   void fadeOn(LedType type, uint16_t fadeTime = DefaultFadeTime);
   void fadeOff(LedType type, uint16_t fadeTime = DefaultFadeTime);
+  void applyFade(LedType type, bool state, uint16_t fadeTime);
 
-  void setBrightness(LedType type, int8_t brightness);
+  void setBrightness(LedType type, uint8_t brightness);
+  void setPwmDuty(LedType type, uint16_t ledDutyCycle);
   void setFrequency(LedType type, int16_t ledFrequency = DefaultLedFrequency);
 
   void blink(LedType type, uint16_t onTime = 1000, uint16_t offTime = 0);
   void breathe(LedType type, int16_t breatheTime = 1000, bool defaultState = 1, bool repeatAgain = 0);
   void breathing(LedType type, int16_t breatheTime = 1000, int16_t delayTime = 0);
 
-  bool getLedState(LedType type);
+  bool getChannel(LedType type);
+
+  struct Led {
+    int8_t pin[4] = { 127, 127, 127, 127 };  // 4 leds can be attached to same type
+    int8_t channel;
+  };
+  Led leds[10];  //  upto 10 types can be used at once
+
+  struct LedChannels {
+    LedType type;
+    int16_t onTime;
+    int16_t offTime;
+    int16_t smoothTime = 500;
+    int16_t ledDutyCycle;
+    int16_t ledFrequency;
+  };
+  LedChannels chData[maxChannels];
 
 private:
-  Led *leds;
-  int8_t numLeds;
+  void _attachPWM(LedType type);
+  void _detachPWM(LedType type);
+  int numLeds;
   int16_t ledResoltuion;
+  uint16_t channelBitMask = 0b0000000000000000;  // track used ledc channels
 };
 
 #endif
